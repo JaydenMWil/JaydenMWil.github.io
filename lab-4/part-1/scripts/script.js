6dda58a43caa4efb4bbab44ff5bf08e5e6b062b6
JaydenMDev <jayden.wilson@dcmail.ca>
2026-02-16 13:00:11 -0500

    changed textContent to be equal to newStory

--- lab-4/part-1/scripts/script.js
+++ lab-4/part-1/scripts/script.js
@@ -68,6 +68,6 @@ function generateStory() {
     }
 
     // TODO: replace "" with the correct expression
-    story.textContent = "";
+    story.textContent = newStory;
     story.style.visibility = "visible";
 }
