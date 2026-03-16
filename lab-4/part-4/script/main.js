a6e011c4370f2762e84628cda9b99ab30e7b1c74
JaydenMDev <jayden.wilson@dcmail.ca>
2026-03-16 14:16:03 -0400

    add exists attribute to Ball, change collisionDetect to check for it

--- lab-4/part-4/script/main.js
+++ lab-4/part-4/script/main.js
@@ -32,10 +32,13 @@ class Shape {
 
 class Ball extends Shape {
 
+  exists;
+
   constructor(x, y, velX, velY, color, size) {
     super(x, y, velX, velY)
     this.color = color;
     this.size = size;
+    this.exists = true;
   }
 
   draw() {
@@ -68,7 +71,7 @@ class Ball extends Shape {
 
   collisionDetect() {
     for (const ball of balls) {
-      if (this !== ball) {
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
