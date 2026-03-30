1ae7944957229a3e1afa2ce0e6c0c9b25f89ef0e
JaydenMDev <jayden.wilson@dcmail.ca>
2026-03-30 14:17:18 -0400

    add comments, change quotes to single quotes, and add ball count <p> tag

--- lab-4/part-4/script/main.js
+++ lab-4/part-4/script/main.js
@@ -1,7 +1,7 @@
 // setup canvas
 
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
@@ -21,7 +21,7 @@ function randomRGB() {
 }
 
 class Shape {
-
+  // constructor class
   constructor(x, y, velX, velY) {
     this.x = x;
     this.y = y;
@@ -31,9 +31,10 @@ class Shape {
 }
 
 class Ball extends Shape {
-
+  // attributes
   exists;
 
+  // constructor
   constructor(x, y, velX, velY, color, size) {
     super(x, y, velX, velY);
     this.color = color;
@@ -41,6 +42,7 @@ class Ball extends Shape {
     this.exists = true;
   }
 
+  // draw method that draws the path in the ctx (canva object)
   draw() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
@@ -48,35 +50,40 @@ class Ball extends Shape {
     ctx.fill();
   }
 
+  // update method that checks the movement and if its colliding with a wall, if it is then it moves it back
   update() {
     if (this.x + this.size >= width) {
       this.velX = -this.velX;
     }
-
     if (this.x - this.size <= 0) {
       this.velX = -this.velX;
     }
-
     if (this.y + this.size >= height) {
       this.velY = -this.velY;
     }
-
     if (this.y - this.size <= 0) {
       this.velY = -this.velY;
     }
-
+    // if it isnt touching any walls then update its x/y by adding the velocity to it
     this.x += this.velX;
     this.y += this.velY;
   }
 
+  // this detects if the ball is coliding with another ball
   collisionDetect() {
+    // loops through our lowkey poorly named array of balls for each ball
     for (const ball of balls) {
+      // checks if this (object) isnt equal to the current ball in the array, and if the ball in the array exists
       if (!(this === ball) && ball.exists) {
+        // creates delta x and delta y, calculate by current ball's x or y and subtract the current balls x and y as well
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
+        // calculates distance from the square root of dx * dx + dy * dy.
         const distance = Math.sqrt(dx * dx + dy * dy);
 
+        // check the if the distance is less than the size of both balls combined
         if (distance < this.size + ball.size) {
+          // if it is change the color to something random
           ball.color = this.color = randomRGB();
         }
       }
@@ -85,51 +92,56 @@ class Ball extends Shape {
 }
 
 class EvilCircle extends Shape {
-
+  // evil class inheriting Shape's
+  // constructor has x, y, velX, velY, color, and size
   constructor(x, y, velX, velY, color, size) {
+    // inherits the x,y from the shape class, it also sets the velx and vely to 20
     super(x, y, 20, 20);
-    this.color = "white";
+    // set the color to white
+    this.color = 'white';
+    // set the size to ten
     this.size = 10;
-    window.addEventListener("keydown", (e) => {
+    // in this constructor we are adding an eventlistener for a keydown to this object we create,
+    // this is tied to the objects creation and we call this lambda method that is a switch case
+    // that checks the keyboard event "e" for the wasd keys, and moves the character in that direction
+    // (i.e a is left, d is right, etc)
+    window.addEventListener('keydown', (e) => {
       switch (e.key) {
-        case "a":
+        case 'a':
           this.x -= this.velX;
           break;
-        case "d":
+        case 'd':
           this.x += this.velX;
           break;
-        case "w":
+        case 'w':
           this.y -= this.velY;
           break;
-        case "s":
+        case 's':
           this.y += this.velY;
           break;
       }
     });
   }
 
+  // inheriting the draw function
   draw() {
     ctx.beginPath();
     ctx.strokeStyle = this.color;
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.stroke();
-    // test value
-    ctx.lineWidth = 3
+    ctx.lineWidth = 3;
   }
 
   checkBounds() {
     if (this.x + this.size >= width) {
       this.x -= this.size;
     }
-
     if (this.x - this.size <= 0) {
       this.x += this.size;
     }
-
     if (this.y + this.size >= height) {
       this.y -= this.size;
     }
-
     if (this.y - this.size <= 0) {
       this.y += this.size;
     }
@@ -148,10 +160,7 @@ class EvilCircle extends Shape {
       }
     }
   }
-
-
 }
-
 
 const balls = [];
 
@@ -165,20 +174,15 @@ while (balls.length < 25) {
     random(-7, 7),
     random(-7, 7),
     randomRGB(),
-    size,
+    size
   );
-
   balls.push(ball);
 }
 
 const evilguy = new EvilCircle(random(0, width), random(0, height));
-
 function loop() {
-  ctx.fillStyle = "rgb(0 0 0 / 25%)";
+  ctx.fillStyle = 'rgb(0 0 0 / 25%)';
   ctx.fillRect(0, 0, width, height);
-
-
-
 
   for (const ball of balls) {
     if (ball.exists) {
@@ -196,5 +200,3 @@ function loop() {
 }
 
 loop();
-
-
